6d27ed7c9c7e1f8f2dd6bbb103733f9f9497fd85
Pallavi Narsinge <pallavinarsinge123@gmail.com>
2026-06-18 11:13:58 +0530

    Enhance portfolio UI with responsive project cards, resume section, education cards, footer, and improved styling

--- StudentPortfolio/urls.py
+++ StudentPortfolio/urls.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from website import views
-
 from django.conf import settings
 from django.conf.urls.static import static
 
+from website.views import home
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name="home"),
+    path("", home, name="home"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
