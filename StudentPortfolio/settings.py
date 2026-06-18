fea0b822d6a89ff587bb57f36a014c7ce7ea78fc
Pallavi Narsinge <pallavinarsinge123@gmail.com>
2026-06-18 13:03:26 +0530

    Add contact form functionality, improve resume section, enhance project cards, and update portfolio UI

--- StudentPortfolio/settings.py
+++ StudentPortfolio/settings.py
@@ -115,3 +115,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "yourgmail@gmail.com"
+
+EMAIL_HOST_PASSWORD = "your_app_password"
