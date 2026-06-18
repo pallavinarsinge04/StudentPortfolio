3c7d6faf868f0fa6e9412eb62aedb27bcc948aad
Pallavi Narsinge <pallavinarsinge123@gmail.com>
2026-06-18 11:30:37 +0530

    Add responsive fixed navbar with smooth scrolling and improved portfolio navigation

--- website/views.py
+++ website/views.py
@@ -1,14 +1,4 @@
 from django.shortcuts import render
-from .models import Project
 
 def home(request):
-
-    projects = Project.objects.all()
-
-    return render(
-        request,
-        "home.html",
-        {
-            "projects": projects
-        }
-    )
+    return render(request, "home.html")
